334deadb903eb6caa5851487513aef1bfceb0c90
Ilya Bondarenko <ilya_bond_2019@mail.ru>
2026-05-27 16:02:10 +0300

    Return unauthorized JSON response

--- server/src/main/kotlin/com/example/nobelapi/plugins/Authentication.kt
+++ server/src/main/kotlin/com/example/nobelapi/plugins/Authentication.kt
@@ -1,10 +1,13 @@
 package com.example.nobelapi.plugins
 
 import com.example.nobelapi.security.JwtConfig
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
+import io.ktor.server.application.call
 import io.ktor.server.application.install
 import io.ktor.server.auth.Authentication
 import io.ktor.server.auth.jwt.jwt
+import io.ktor.server.response.respond
 
 fun Application.configureAuthentication(jwtConfig: JwtConfig) {
     install(Authentication) {
@@ -12,6 +15,9 @@ fun Application.configureAuthentication(jwtConfig: JwtConfig) {
             realm = jwtConfig.realm
             verifier(jwtConfig.verifier)
             validate { credential -> jwtConfig.validate(credential) }
+            challenge { _, _ ->
+                call.respond(HttpStatusCode.Unauthorized, mapOf("error" to "Unauthorized"))
+            }
         }
     }
 }
